1be0f24b5a69b317eb6b667d2c68e7bca856d747
Łukasz Staniszewski <lukaszstaniszewski10@gmail.com>
2022-05-29 20:08:26 +0200

    prefinal experiments

--- model_scripts/generate_fairseq.sh
+++ model_scripts/generate_fairseq.sh
@@ -1,13 +1,18 @@
 # !/bin/bash
 
+MODEL_NAME = lstm
+# MODEL_NAME = transformer_tiny
+
+mkdir ./../predictions/outs_$MODEL_NAME
+mkdir ./../logs/logs_$MODEL_NAME
+
+
 fairseq-generate ../data/model_data/binarized \
     --source-lang ar \
     --target-lang en \
-    --results-path ./outs \
-    --path ./checkpoints/lstm/checkpoint_best.pt \
+    --results-path ./../predictions/outs_$MODEL_NAME \
+    --path ./checkpoints/$MODEL_NAME/checkpoint_best.pt \
     --batch-size 128 \
     --beam 5 \
-    --tensorboard-logdir ./../logs/tensorboard \
+    --tensorboard-logdir ./../logs/logs_$MODEL_NAME \
     --remove-bpe=sentencepiece
-    # --scoring sacrebleu
-    # --scoring chrf
--- model_scripts/train_fairseq.sh
+++ model_scripts/train_fairseq.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 MODEL_DATA=../data/model_data
 BIN=$MODEL_DATA/binarized
-mkdir -p checkpoints/lstm
-mkdir -p checkpoints/transformer_tiny
+
+mkdir -p ./../checkpoints/lstm
+mkdir -p ./../checkpoints/transformer_tiny
+mkdir ./../logs/logs_$MODEL_NAME
+
+MODEL_NAME = lstm
+# MODEL_NAME = transformer_tiny
 
 fairseq-train $BIN \
-  --arch lstm --batch-size 256 \
+  --arch $MODEL_NAME --batch-size 256 \
   --optimizer nag --lr 0.15 \
-  --save-dir checkpoints/lstm \
+  --save-dir ./../checkpoints/$MODEL_NAME \
   --clip-norm 0.1 --dropout 0.2 \
-  --user-dir tb_lstm \
   --seed 0 \
-  --tensorboard-logdir tb_lstm 
-
-# fairseq-train $BIN \
-  # --arch transformer_tiny --batch-size 256 \
-  # --optimizer nag --lr 0.15 \
-  # --save-dir checkpoints/transformer_tiny \
-  # --clip-norm 0.1 --dropout 0.2 \
-  # --user-dir tb \
-  # --seed 0 \
-  # --tensorboard-logdir tb
-
+  --tensorboard-logdir ./../logs/logs_$MODEL_NAME
